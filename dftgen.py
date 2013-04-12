b7cf62fb550503a3e420cb6434b8ca1cab47b3c3
Howard Mao <zhehao.mao@gmail.com>
2013-04-12 15:27:20 -0400

    make precision for dftgen configurable

--- dftgen.py
+++ dftgen.py
@@ -8,7 +8,8 @@ from romgen import convert_int_array
 
 if __name__ == '__main__':
     N = 16
+    prec = 16
     coeffs = [math.exp(-2 * math.pi * k * n / float(N)) 
                 for (k, n) in itertools.product(range(0, N), range(0, N))]
-    intcoeffs = [int((2 ** 15 - 1) * c) for c in coeffs]
-    print "(" + convert_int_array(intcoeffs, 16) + ")"
+    intcoeffs = [int((2 ** (prec - 1) - 1) * c) for c in coeffs]
+    print "(" + convert_int_array(intcoeffs, prec) + ")"

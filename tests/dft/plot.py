c2896c5ac48da584e1b89e43a62fc309ba79ccb8
Howard Mao <zhehao.mao@gmail.com>
2013-04-12 21:05:08 -0400

    plot computes amplitude from inputs

--- tests/dft/plot.py
+++ tests/dft/plot.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import math
+import sys
 
 if __name__ == '__main__':
-    vals = [input() for i in range(0, 16)]
-    plt.plot(vals)
+    vals = [tuple([int(i) for i in s.strip().split(' ')]) for s in sys.stdin]
+    plt.plot([math.sqrt(r ** 2 + i ** 2) for (r, i) in vals])
     plt.show()

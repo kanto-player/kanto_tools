fc857c0fbb06b58cbdc60558e3c33b2eded6f0e0
Howard Mao <zhehao.mao@gmail.com>
2013-04-15 16:41:49 -0400

    allow plot.py to plot both real and imaginary numbers

--- fft/plot.py
+++ fft/plot.py
@@ -3,6 +3,15 @@ import math
 import sys
 
 if __name__ == '__main__':
-    vals = [tuple([int(i) for i in s.strip().split(' ')]) for s in sys.stdin]
-    plt.plot([math.sqrt(r ** 2 + i ** 2) for (r, i) in vals])
+    imag = False
+    if len(sys.argv) >= 2 and sys.argv[1] == "imag":
+        imag = True
+
+    if imag:
+        vals = [tuple([int(i) for i in s.strip().split(' ')]) for s in sys.stdin]
+        plt.plot([math.sqrt(r ** 2 + i ** 2) for (r, i) in vals])
+    else:
+        vals = [int(s.strip()) for s in sys.stdin]
+        plt.plot(vals)
     plt.show()
+

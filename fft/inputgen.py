e9d0a54db8e61b06cc1c9f5dbeb7e1c57db57a9e
Howard Mao <zhehao.mao@gmail.com>
2013-04-15 15:40:32 -0400

    let inputgen generate variable number of samples

--- fft/inputgen.py
+++ fft/inputgen.py
@@ -1,9 +1,12 @@
 import numpy as np
+import sys
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: " + sys.argv[0] + " N")
     fs = 44100.
     W = 10000
-    N = 16
+    N = int(sys.argv[1])
     i = np.array(range(0, N))
     t = i / fs
     x = (2 ** 15 - 1) * np.cos(2 * np.pi * W * t)

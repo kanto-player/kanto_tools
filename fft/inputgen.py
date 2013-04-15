d60bf7e9f45a345dee29de051aacf817acda5759
Howard Mao <zhehao.mao@gmail.com>
2013-04-15 16:40:05 -0400

    inputgen has varying frequency

--- fft/inputgen.py
+++ fft/inputgen.py
@@ -2,11 +2,12 @@ import numpy as np
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: " + sys.argv[0] + " N")
+    if len(sys.argv) < 3:
+        print("Usage: " + sys.argv[0] + " N W")
+        sys.exit(1)
     fs = 44100.
-    W = 10000
     N = int(sys.argv[1])
+    W = float(sys.argv[2])
     i = np.array(range(0, N))
     t = i / fs
     x = (2 ** 15 - 1) * np.cos(2 * np.pi * W * t)

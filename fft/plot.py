859413a04fb1d8b31b55cf6a270ebe626ee734ab
Howard Mao <zhehao.mao@gmail.com>
2013-04-24 14:40:27 -0400

    should be complex not imaginary

--- fft/plot.py
+++ fft/plot.py
@@ -3,11 +3,11 @@ import math
 import sys
 
 if __name__ == '__main__':
-    imag = False
-    if len(sys.argv) >= 2 and sys.argv[1] == "imag":
-        imag = True
+    comp = False
+    if len(sys.argv) >= 2 and sys.argv[1] == "comp":
+        comp = True
 
-    if imag:
+    if comp:
         vals = [tuple([int(i) for i in s.strip().split(' ')]) for s in sys.stdin]
         plt.plot([math.sqrt(r ** 2 + i ** 2) for (r, i) in vals])
     else:

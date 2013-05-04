7614a54c6133877c0541e6e26ef246f46be09486
Howard Mao <zhehao.mao@gmail.com>
2013-05-04 11:00:16 -0400

    change interface of sumbins

--- fft/sumbins.c
+++ fft/sumbins.c
@@ -1,15 +1,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	int i;
+	int i, n, res;
 	unsigned int sum = 0;
-	short real, imag;
+	short real;
 
-	for (i = 0; i < 256; i++) {
-		fscanf(stdin, "%hd %hd\n", &real, &imag);
-		if (i % 16 == 0) {
+	if (argc < 2) {
+		fprintf(stderr, "Usage: sumbins n\n");
+		exit(EXIT_FAILURE);
+	}
+
+	n = atoi(argv[1]);
+
+	for (i = 0; ; i++) {
+		res = fscanf(stdin, "%hd\n", &real);
+		
+		if (res == 0 || res == EOF)
+			break;
+		else if (res < 0) {
+			perror("fscanf");
+			exit(EXIT_FAILURE);
+		}
+
+		if (i % n == 0 && i != 0) {
 			printf("%d\n", sum);
 			sum = 0;
 		}
@@ -19,6 +34,8 @@ int main(void)
 		else
 			sum += real;
 	}
+
+	printf("%d\n", sum);
 
 	return 0;
 }

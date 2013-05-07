36ea9fccb203209b9c785a80363bb7ac1ed80e47
Amrita Mazumdar <amritamaz@gmail.com>
2013-05-06 22:55:55 -0400

    added python script to convert mp3 files to pcm data to be written to an sd card

---
+++ sdcard/mkauimg.py
@@ -0,0 +1,33 @@
+# Usage: ./mkauimg.py audio1.ogg [audio2.mp3 ...] output.raw
+# Takes in several audio files, concatenates them, and converts
+# then into raw 16-bit signed-integer big-endian PCM data.
+#
+# First words contain the lengths of all the files.
+
+import sys
+import subprocess
+import os
+import struct
+
+filesizelist = []
+
+for i, inptfile in enumerate(sys.argv[1:-1]):
+  # convert to PCM using sox
+  sox_call = "sox \"" + inptfile + "\" -b 16 -e signed-integer -B -c 1 -t raw - " + "> temp" + str(i)
+  
+  os.system(sox_call)
+
+# get number of characters, need to convert to 512byte block
+  filelength = os.path.getsize("temp" + str(i))
+  newlength = ((filelength - 1)/512 + 1)
+  filesizelist.append(newlength)
+
+header = ''.join([struct.pack(">H", l) for l in filesizelist])
+header = header.ljust(512, '\0')
+f = open(sys.argv[-1],"a")
+f.write(header)
+
+for i, inptfile in enumerate(sys.argv[1:-1]):
+  fin = open("temp" + str(i), "r")
+  data = fin.read()
+  f.write(data)
